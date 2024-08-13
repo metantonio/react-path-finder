e469368c590b1991d1680d6d6e89e10e5a61e291
unknown <antonio_martinez88@hotmail.com>
2024-08-13 14:54:33 -0400

    upd

---
+++ src/pages/components/CanvasExample.js
@@ -0,0 +1,239 @@
+import React, { useEffect, useRef, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import circleFun from "./utils/circleForCanvas";
+import lineFun from "./utils/lineForCanvas";
+import arrowFun from "./utils/arrowForCanvas";
+import dijkstraAlgo from "./utils/algorithms/DijkstraAlgo";
+
+const useStyle = makeStyles({
+	canvasStyle: ({ arrowName }) => ({
+		marginTop: "10px",
+		cursor: arrowName ? "grabbing" : "default",
+	}),
+});
+
+const CanvasExample = ({ reload, setArr }) => {
+	const canvasRef = useRef(null);
+	const [resultArr, setResultArr] = useState([]);
+	const [element, elementArr] = useState([]);
+	const [arrows, setArrows] = useState([]);
+	const [start, setStart] = useState({ x: 10, y: 14.5 });
+	const [end, setEnd] = useState({ x: 40, y: 10 });
+	const [startLoctionForDijkstra, setStartLocation] = useState(null);
+	const [finishLoctionForDijkstra, setFinishLoaction] = useState(null);
+	const [dragDeatils, setDragDetails] = useState({
+		arrowName: undefined,
+	});
+	const { canvasStyle } = useStyle(dragDeatils);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight / 1.3;
+		canvas.style.width = `${window.innerWidth}px`;
+		canvas.style.height = `${window.innerHeight / 1.3}px`;
+		canvas.style.background = "#32567a";
+		const ctx = canvas.getContext("2d");
+		const Circle = circleFun(ctx);
+		const Line = lineFun(ctx);
+		const Arrow = arrowFun(ctx);
+
+		//Lines
+		const drawLines = [
+			new Line({ x: 100, y: 160 }, { x: 100, y: 140 }), //1 to 4
+			new Line({ x: 100, y: 140 }, { x: 100, y: 120 }), //4 to 8
+			new Line({ x: 100, y: 140 }, { x: 200, y: 140 }), //4 to 5
+			new Line({ x: 200, y: 160 }, { x: 200, y: 140 }), //2 to 5
+			new Line({ x: 300, y: 160 }, { x: 300, y: 140 }), //3 to 6
+			new Line({ x: 200, y: 140 }, { x: 300, y: 140 }), //5 to 6
+			new Line({ x: 300, y: 120 }, { x: 300, y: 140 }), //7 to 6
+			new Line({ x: 200, y: 140 }, { x: 200, y: 60 }), //5 to 10
+			new Line({ x: 100, y: 60 }, { x: 200, y: 60 }), //9 to 10
+			new Line({ x: 300, y: 60 }, { x: 200, y: 60 }), //11 to 10
+			new Line({ x: 200, y: 20 }, { x: 200, y: 60 }), //12 to 10
+			new Line({ x: 200, y: 20 }, { x: 300, y: 20 }), //12 to 13
+			new Line({ x: 200, y: 20 }, { x: 200, y: 0 }), //12 to 15
+			new Line({ x: 200, y: 20 }, { x: 100, y: 20 }), //12 to 14
+		]
+		
+		//------------------------------------------------------------------
+
+		const lines = [
+			...drawLines
+		];
+
+		//Circle drawing
+
+		const circleColor = "#f1f1f1";
+
+		const a = new Circle(100, 160, circleColor, "a");
+		const b = new Circle(100, 140, circleColor, "b");
+		const c = new Circle(100, 120, circleColor, "c");
+		const d = new Circle(200, 160, circleColor, "f");
+		const e = new Circle(200, 140, circleColor, "g");
+		const f = new Circle(300, 160, circleColor, "k");
+		const g = new Circle(300, 140, circleColor, "l");
+		const h = new Circle(100, 60, circleColor, "d");
+		const i = new Circle(300, 120, circleColor, "m");
+		const j = new Circle(200, 60, circleColor, "h");
+		const k = new Circle(300, 60, circleColor, "n");
+		const l = new Circle(200, 20, circleColor, "i");
+		const m = new Circle(300, 20, circleColor, "o");
+		const n = new Circle(100, 20, circleColor, "e");
+		const o = new Circle(200, 0, circleColor, "j");
+		
+		//------------------------------------------------------------------------
+
+		const circles = [
+			a,
+			b,
+			c,
+			d,
+			e,
+			f,
+			g,
+			h,
+			i,
+			j,
+			k,
+			l,
+			m,
+			n,
+			o,
+		];
+
+		//drawing Lines
+		lines.forEach((x) => x.draw());
+		//drawing the circle
+		circles.forEach((x) => x.draw());
+
+		//Creating Arrow Objects
+		let fisrtArrow = new Arrow(16, "#16fb04", "startArrow");
+		let lastArrow = new Arrow(20, "red", "endArrow");
+
+		fisrtArrow.move(start.x, start.y);
+		lastArrow.move(end.x, end.y);
+
+		setArrows([fisrtArrow, lastArrow]);
+
+		elementArr(circles);
+
+		//lines.forEach((x) => console.log(x.weight()));
+
+		const Graph = dijkstraAlgo();
+		const graph = new Graph();
+
+		//add Vertex
+
+		graph.addVartex("A");
+		graph.addVartex("B");
+		graph.addVartex("C");
+		graph.addVartex("D");
+		graph.addVartex("E");
+		graph.addVartex("F");
+		graph.addVartex("G");
+		graph.addVartex("H");
+		graph.addVartex("I");
+		graph.addVartex("J");
+		graph.addVartex("K");
+		graph.addVartex("L");
+		graph.addVartex("M");
+		graph.addVartex("N");
+		graph.addVartex("O");
+
+		// add Edge
+		/* for(let i=0; i< drawLines.length; i++){
+			graph.addEdge(`A${i}`, `B${i}`, drawLines[i]);
+		} */
+		
+
+		graph.addEdge(`A`, `B`, drawLines[0]);
+		graph.addEdge(`B`, `C`, drawLines[1]);
+		graph.addEdge(`B`, `G`, drawLines[2]);
+		graph.addEdge(`F`, `G`, drawLines[3]);
+		graph.addEdge(`K`, `L`, drawLines[4]);
+		graph.addEdge(`G`, `L`, drawLines[5]);
+		graph.addEdge(`L`, `M`, drawLines[6]);
+		graph.addEdge(`G`, `H`, drawLines[7]);
+		graph.addEdge(`D`, `H`, drawLines[8]);
+		graph.addEdge(`H`, `N`, drawLines[9]);
+		graph.addEdge(`H`, `I`, drawLines[10]);
+		graph.addEdge(`I`, `O`, drawLines[11]);
+		graph.addEdge(`I`, `J`, drawLines[12]);
+		graph.addEdge(`E`, `I`, drawLines[13]);
+		
+
+		const findArr = graph.dijkstra(
+			startLoctionForDijkstra,
+			finishLoctionForDijkstra
+		);
+		// Saving the result arr
+		setResultArr(findArr);
+	}, [reload, start, end, startLoctionForDijkstra, finishLoctionForDijkstra]);
+
+	//passing the result arr in parent
+	setArr(resultArr);
+
+	//Mouse Down
+
+	const canvasMouseDown = (e) => {
+		const rect = canvasRef.current.getBoundingClientRect();
+		const mouseX = e.clientX - rect.left;
+		const mouseY = e.clientY - rect.top;
+
+		//Cheaking ... is 'Click' events occurs upon arrows or not?
+		arrows.forEach((x) => {
+			if (x.click(mouseX, mouseY)) {
+				setDragDetails({ arrowName: x.name });
+			}
+		});
+	};
+
+	const canvasMouseMove = (e) => {
+		const rect = canvasRef.current.getBoundingClientRect();
+		const mouseX = e.clientX - rect.left;
+		const mouseY = e.clientY - rect.top;
+
+		//Dragging Arrows Logic
+		arrows.forEach((x) => {
+			if (dragDeatils.arrowName === "startArrow") {
+				setStart({ x: mouseX, y: mouseY });
+			}
+			if (dragDeatils.arrowName === "endArrow") {
+				setEnd({ x: mouseX, y: mouseY });
+			}
+		});
+	};
+
+	const canvasMouseUp = (e) => {
+		setDragDetails({ arrowName: undefined });
+		setStartLocation(null);
+		setFinishLoaction(null);
+
+		element.forEach((x) => {
+			arrows.forEach((arrow) => {
+				x.click(
+					arrow.downPointX,
+					arrow.downPointY,
+					arrow.name,
+					setStartLocation,
+					setFinishLoaction
+				);
+			});
+		});
+	};
+
+	return (
+		<>
+			<canvas
+				ref={canvasRef}
+				className={canvasStyle}
+				onMouseDown={canvasMouseDown}
+				onMouseUp={canvasMouseUp}
+				onMouseMove={canvasMouseMove}
+			/>
+		</>
+	);
+};
+
+export default CanvasExample;
